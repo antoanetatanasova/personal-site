26c82144b964539ef221ee00b8de107bbb2e9642
Antoaneta Atanasova <antoanetatanasova@gmail.com>
2021-03-15 18:30:08 +0200

    Background image added for Recent Article section, some redundant files are removed

--- app/assets/scripts/modules/FooterComponent.js
+++ app/assets/scripts/modules/FooterComponent.js
@@ -7,12 +7,12 @@ function FooterComponent() {
                 Follow me or contact me:
             </p>
             <ul className="footer--contact-info">
-                <li className="footer--contact-info-item"><a className="footer--contact-info-item-link" href="#"><i class="fab fa-researchgate"></i></a></li>
+                <li className="footer--contact-info-item"><a className="footer--contact-info-item-link" href="https://www.researchgate.net/profile/Antoaneta-Atanasova"><i class="fab fa-researchgate"></i></a></li>
                 <li className="footer--contact-info-item"><a className="footer--contact-info-item-link" href="https://github.com/antoanetatanasova"><i class="fab fa-github"></i></a></li>
                 <li className="footer--contact-info-item"><a className="footer--contact-info-item-link" href="https://www.linkedin.com/in/antoanetatanasova/"><i class="fab fa-linkedin"></i></a></li>
             </ul>
             <p>
-                Copyright &copy; 2021 All rights reserved.
+                Copyright &copy; 2021. All rights reserved
             </p>
         </footer>
     );
