de2dcc0bb5719f5a6cc13002c2652a44c73bc6b6
Antoaneta Atanasova <antoanetatanasova@gmail.com>
2021-03-16 20:34:25 +0200

    added the AboutComponent. Home page is complete.

---
+++ app/assets/scripts/AboutComponent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const AboutComponent = () => {
+    return (
+        <section className="about">
+            <img className="about-profile-picture" src="https://res.cloudinary.com/the-potential/image/upload/v1615903169/personal-site/antoinette_ezrg3f.jpg"></img>
+            <article className="about-profile-description">
+                <p>Hi, my name is Antoaneta. I was born in Sofia, but at some point of my live I had the opportunity to live in two of the most intriguing cities of the world: Paris and Istanbul. I reached to these cities because of the desire to read more than anyone else in the world and even to read what no-one has read before me. This dream was fully accomplished when in 2015 I worked on creating the inventory of the Archive of the American Board of Missions. At that time I was the first one who opened some of the documents, forgotten long ago.</p>
+            </article>
+        </section>
+    );
+}
+
+export default AboutComponent;
--- app/assets/scripts/modules/HomePageComponent.js
+++ app/assets/scripts/modules/HomePageComponent.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import RecentArticleComponent from './RecentArticleComponent'
 import InterestsComponent from './InterestsComponent'
+import AboutComponent from '../AboutComponent';
 
 const HomePageComponent = () => {
     return (
     <>
         <RecentArticleComponent />
         <InterestsComponent />
+        <AboutComponent />
     </>
     );
 }
