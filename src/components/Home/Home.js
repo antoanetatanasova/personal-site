857a4869cb7913c9ceea72afee2944f6d935b786
Antoaneta Atanasova <antoanetatanasova@gmail.com>
2021-03-27 12:59:16 +0200

    Interests section completed

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Featured from './../../components/Featured/Featured';
+import Interests from './../../components/Interests/Interests';
 
 const Home = () => {
     return (
         <>
             <Featured />
+            <Interests />
 
         </>
     )
---
+++ src/components/Interests/Interests.js
@@ -0,0 +1,25 @@
+import style from './../../components/Interests/Interests.module.css';
+
+const Interests = () => {
+    return (
+        <section className={style.interests}>
+            <ul className={style.interestsList}>
+                <li className={style.interestsListItem}>
+                    <h4>History</h4>
+                    <p>My major research interests are directed towards the European models of modernization of the Ottoman Empire during the Eighteenth Century, as well as toward the European-Ottoman relations interconected with those modernization processes.</p>
+                </li>
+                <li className={style.interestsListItem}>
+                    <h4>Translation studies</h4>
+                    <p>I am tempted to explain the strategies to translate the “untranslatability”: lacunas or liguistic gaps. Part of my research interest in focused on explaining the ways to translate the culture-specific material elements or realia.</p>
+                </li>
+                <li className={style.interestsListItem}>
+                    <h4>Translation</h4>
+                    <p>I take pride in being a published translator, when in 2013 I took part in translating a section of Pierre Michon's book <span>Corps du roi</span>. My prefered language pair is French-Bulgarian.</p>
+                </li>
+            </ul>
+
+        </section>
+    )
+}
+
+export default Interests;
