a73d787caa984246ea81d9349f3cb6f4820ec3b8
Antoaneta Atanasova <antoanetatanasova@gmail.com>
2021-04-09 20:30:25 +0300

    logo book icon added

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,11 +1,12 @@
 import style from './Logo.module.css';
+import {GiBookmarklet} from 'react-icons/gi'
 
 const Logo = () => {
     const siteTitle = "Antoaneta Atanasova";
 
     return (
         <>
-            <h1 className={style.logo}><a href='/'>{siteTitle}</a></h1>
+            <h1 className={style.logo}><a style={{"color": "#0683D7"}} href='/'>{siteTitle} <GiBookmarklet /></a></h1>
         </>
     );
 }
