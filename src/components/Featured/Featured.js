aceee1f269413194c38337c2e899e8d33bf90eb7
Antoaneta Atanasova <antoanetatanasova@gmail.com>
2021-04-12 18:39:44 +0300

    restyling the Home page

--- src/components/Featured/Featured.js
+++ src/components/Featured/Featured.js
@@ -8,15 +8,16 @@ const Featured = () => {
         bloomsburyUrl: "https://www.bloomsbury.com/uk/european-revolutions-and-the-ottoman-balkans-9780755603282/",
         googlebooksUrl: "https://books.google.bg/books?hl=en&lr=&id=wWjDDwAAQBAJ&oi=fnd&pg=PA65&ots=MAUAUK229u&sig=XpWo7E4YHIgI1MMlnKe_KWHZk_0&redir_esc=y#v=onepage&q&f=false",
     }
+
     return (
         <article className={style.featured}>
-                <h4 className={style.featuredSectionTitle}>Recent Publication</h4>
             <div className={style.featuredWrapper}>
+                <h4 className={style.featuredSectionTitle}>Featured Publication</h4>
                 <h2 className={style.featuredMainTitle}>Military Reforms as a Diplomatic Bargaining Chip</h2>
                 <h3>French-Ottoman Relations at the End of the Eighteenth Century</h3>
                 <a href={links.bloomsburyUrl}> <Button text="More Info" /></a>
                 <a href={links.googlebooksUrl}> <Button text="Read it" /></a>
-                <h5>in: European Revolutions and the Ottoman Balkans Nationalism, Violence and Empire in the Long Nineteenth-Century, Editor: Dimitris Stamatopoulos</h5>
+                <h5 className={style.featuredSectionFooter}>in: European Revolutions and the Ottoman Balkans Nationalism, Violence and Empire in the Long Nineteenth-Century, Editor: Dimitris Stamatopoulos</h5>
             </div >
         </article >
     )
--- src/components/Interests/Interests.js
+++ src/components/Interests/Interests.js
@@ -1,6 +1,6 @@
 import style from './../../components/Interests/Interests.module.css';
-import { FaUniversity } from 'react-icons/fa'
-import { GiArchiveResearch } from 'react-icons/gi'
+// import { FaUniversity } from 'react-icons/fa'
+import { GiArchiveResearch, GiAncientColumns } from 'react-icons/gi'
 import { IoLanguage } from 'react-icons/io5'
 
 const Interests = () => {
@@ -9,17 +9,17 @@ const Interests = () => {
             <ul className={style.interestsList}>
                 <li className={style.interestsListItem}>
                     <h4>History</h4>
-                    <p className={style.interestsListIcon} style={{ "textAlign": "center" }}><FaUniversity /></p>
+                    <GiAncientColumns className={style.interestsListIcon} />
                     <p>My major research interests are directed towards the European models of modernization of the Ottoman Empire during the Eighteenth Century, as well as toward the European-Ottoman relations interconected with those modernization processes.</p>
                 </li>
                 <li className={style.interestsListItem}>
                     <h4>Translation studies</h4>
-                    <p className={style.interestsListIcon} style={{ "textAlign": "center" }}><GiArchiveResearch /></p>
+                    <GiArchiveResearch className={style.interestsListIcon} />
                     <p>I am tempted to explain the strategies to translate the “untranslatability”: lacunas or liguistic gaps. Part of my research interest in focused on explaining the ways to translate the culture-specific material elements or realia.</p>
                 </li>
                 <li className={style.interestsListItem}>
                     <h4>Translation</h4>
-                    <p className={style.interestsListIcon} style={{ "textAlign": "center" }}><IoLanguage /></p>
+                    <IoLanguage className={style.interestsListIcon}  />
                     <p>I take pride in being a published translator, when in 2013 I took part in translating a section of Pierre Michon's book <span>Corps du roi</span>. My prefered language pair is French-Bulgarian.</p>
                 </li>
             </ul>
